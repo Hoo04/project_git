3868d6d08ea5d2b98f5522428b0c7bf4c336b22d
sena <cocoavocado0323@gmail.com>
2025-11-27 11:56:00 +0900

    push test

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -3,7 +3,11 @@ package chapter02;
 public class Branch01{
 
     public Branch01(){
+<<<<<<< HEAD
+        System.out.println(x: "충돌 테스트");
+=======
         System.out.println(x: "구글 브랜치에서 작업");
+>>>>>>> google
          System.out.println(x: "다음 브랜치에서 작업");
     }
 }
