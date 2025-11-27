4d23a3e05ef30dcef06724f58850f5b474c9ae89
sena <cocoavocado0323@gmail.com>
2025-11-27 10:46:14 +0900

    version3_d

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -3,6 +3,7 @@ package chapter02;
 public class Branch01{
 
     public Branch01(){
-        System.out.println(x: "main 브랜치에서 수정");
+        System.out.println(x: "main 브랜치에서 작업");
+         System.out.println(x: "다음 브랜치에서 작업");
     }
 }
